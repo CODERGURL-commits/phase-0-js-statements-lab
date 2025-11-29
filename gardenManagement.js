3a2e87a37816ca95ef545e6f05567f8e26d0c23e
CODERGURL-commits <nafulasandra23@gmail.com>
2025-11-29 04:45:48 +0300

    initial commit

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,28 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+// watering control
+if(temperature > 80) {
+    console.log("Watering on");
+}
+else{
+    console.log("watering off");
+}
+
+// Garden lights control.
+if(timeOfDay === "evening"){
+    console.log("Lights on");
+}
+else{
+    console.log("Lights off");
+}
+//  Soil Moisture Adjustment
+while (soilMoisture < 40) {
+    soilMoisture += 5;
+    if (soilMoisture > 40) {
+        soilMoisture = 40;
+    }
+    console.log(soilMoisture);
+}
+    // Add loop logic here
